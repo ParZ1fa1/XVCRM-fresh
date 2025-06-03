6d5a28a0601b4192b6b7d69c9563ce0593aebdc3
ParZ1fa1 <116948413+ParZ1fa1@users.noreply.github.com>
2025-06-04 02:58:02 +0400

    Delete backend/db.ts

--- backend/db.ts
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-
-export const db = mysql.createPool({
-  host: 'db5017842634.hosting-data.io',
-  user: 'dbu2412816',
-  password: 'fwP5t3Qe!MnpYTq',
-  database: 'dbs14225309',
-  port: 3306,
-});
